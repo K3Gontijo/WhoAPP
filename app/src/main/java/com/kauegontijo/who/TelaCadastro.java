9328b5e2efe064867c81b5d6ce3e9c8322e4a265
Kauê Gontijo <kauegontijo@gmail.com>
2023-04-04 22:56:09 -0300

    update 04/04/2023 -adicionado o banco de dados Firebase -Ajustes em alguns designs

--- app/src/main/java/com/kauegontijo/who/TelaCadastro.java
+++ app/src/main/java/com/kauegontijo/who/TelaCadastro.java
@@ -3,6 +3,7 @@ package com.kauegontijo.who;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class TelaCadastro extends AppCompatActivity {
 
--- app/src/main/java/com/kauegontijo/who/TelaLogin.java
+++ app/src/main/java/com/kauegontijo/who/TelaLogin.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TelaLogin extends AppCompatActivity {
+
+    private EditText editEmail;
+    private EditText editSenha;
+    private Button btnEntrar;
+    private TextView txtCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +23,26 @@ public class TelaLogin extends AppCompatActivity {
         setContentView(R.layout.tela_login);
         getSupportActionBar().hide(); //PARA ESCONDER A BARRA DO TÍTULO
 
-    }
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        txtCadastrar = findViewById(R.id.txtCadastrar);
 
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editEmail.getText().toString();
+                String senha = editSenha.getText().toString();
+
+                Log.i("Teste", email);
+                Log.i("Teste", senha);
+            }
+        });
+
+    }
+    //METODO ADICIONADO NO txtCastrar PARA MANDAR PARA OUTRA TELA
     public void IrTelaCadastro (View v){
-        Intent telaCadstro = new Intent(this,TelaCadastro.class);
-        startActivity(telaCadstro);
+        Intent telaCadastro = new Intent(this,TelaCadastro.class);
+        startActivity(telaCadastro);
     }
 }
